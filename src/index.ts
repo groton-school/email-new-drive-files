59787561fad48209a0d698f9c842409d299bf31a
Seth Battis <sbattis@groton.org>
2024-02-27 13:08:45 -0500

    reset triggers on update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import g from '@battis/gas-lighter';
+import pkg from '../package.json';
 
 type FolderConfig = {
   email: string;
@@ -6,29 +7,57 @@ type FolderConfig = {
   separate?: boolean;
 };
 
+type FileSummary = {
+  name: string;
+  id: string;
+  sent: boolean;
+};
+
+type FolderSummary = {
+  name: string;
+  id: string;
+  files: FileSummary[];
+};
+
+type CheckSummary = {
+  folders: FolderSummary[];
+  attempted: number;
+  sent: number;
+};
+
 const TRIGGER = 'trigger';
+const TRIGGER_VERSION = 'triggerVersion';
 const LAST_CHECK = 'lastCheck';
+const LAST_CHECK_SUMMARY = 'lastCheckSummary';
+const PROPERTIES = [TRIGGER, TRIGGER_VERSION, LAST_CHECK, LAST_CHECK_SUMMARY];
 
 function getFolderIds() {
   return PropertiesService.getUserProperties()
     .getKeys()
-    .filter((key) => key !== TRIGGER && key !== LAST_CHECK);
+    .filter((key) => PROPERTIES.indexOf(key) === -1);
 }
 
 global.onHomepage = (e) => {
   const stats = [];
   const hasTrigger = !!g.PropertiesService.getUserProperty(TRIGGER);
-  stats.push(`Hourly check is${hasTrigger ? '' : ' not'} active`);
+  let triggerVersion = g.PropertiesService.getUserProperty(TRIGGER_VERSION);
+  if (triggerVersion !== pkg.version) {
+    triggerVersion = resetTriggerToCurrentAddOnVersion();
+  }
+  stats.push(
+    `Hourly check is${hasTrigger ? '' : ' not'} active (${triggerVersion})`
+  );
   if (hasTrigger) {
     stats.push(
       `Last checked: ${new Date(
         g.PropertiesService.getUserProperty(LAST_CHECK)
       ).toLocaleString()}`
     );
+    stats.push(...displaySummary());
     stats.push(
       g.CardService.Widget.newTextButton({
         text: 'Check now',
-        functionName: 'hourly'
+        functionName: 'checkNow'
       })
     );
   } else {
@@ -40,7 +69,6 @@ global.onHomepage = (e) => {
       'https://groton-school.github.io/email-new-drive-files/store/card-banner-assets/cardservice-header.png'
     )
   );
-
   const folders = getFolderIds().map((folder) =>
     g.CardService.Widget.newTextButton({
       text: DriveApp.getFolderById(folder).getName(),
@@ -171,16 +199,7 @@ global.save = (e) => {
   for (const id in folders) {
     g.PropertiesService.setUserProperty(id, folders[id]);
   }
-
-  if (!g.PropertiesService.getUserProperty(TRIGGER)) {
-    g.PropertiesService.setUserProperty(LAST_CHECK, new Date().toISOString());
-    const trigger = ScriptApp.newTrigger('hourly')
-      .timeBased()
-      .everyHours(1)
-      .create();
-    g.PropertiesService.setUserProperty(TRIGGER, trigger.getUniqueId());
-  }
-
+  resetTriggerToCurrentAddOnVersion();
   return g.CardService.Card.create({
     header: 'Saved',
     widgets: [
@@ -189,9 +208,63 @@ global.save = (e) => {
   });
 };
 
+function displaySummary(): string[] {
+  const summary: CheckSummary =
+    g.PropertiesService.getUserProperty(LAST_CHECK_SUMMARY);
+  return summary.folders.map(
+    (folder) =>
+      `${folder.files.length} new files in ${folder.name}${
+        folder.files.length > 0
+          ? ` (${folder.files
+              .map(
+                (file) =>
+                  `<a href="https://drive.google.com/file/d/${file.id}">${file.name}</a>`
+              )
+              .join(', ')})`
+          : ''
+      }.`
+  );
+}
+
+global.checkNow = () => {
+  global.hourly();
+  const lastCheck = new Date(
+    g.PropertiesService.getUserProperty(LAST_CHECK)
+  ).toLocaleString();
+  return g.CardService.Card.create({
+    header: `Checked at ${lastCheck}.`,
+    widgets: [
+      ...displaySummary(),
+      g.CardService.Widget.newTextButton({ text: 'Ok', functionName: 'home' })
+    ]
+  });
+};
+
+function resetTriggerToCurrentAddOnVersion() {
+  // delete old trigger
+  const triggerid = g.PropertiesService.getUserProperty(TRIGGER);
+  if (triggerid) {
+    ScriptApp.getProjectTriggers().forEach((trigger) => {
+      if (trigger.getUniqueId() === triggerid) {
+        ScriptApp.deleteTrigger(trigger);
+      }
+    });
+  }
+  global.hourly();
+  // create a new trigger
+  const trigger = ScriptApp.newTrigger('hourly')
+    .timeBased()
+    .everyHours(1)
+    .create();
+  g.PropertiesService.setUserProperty(TRIGGER, trigger.getUniqueId());
+  g.PropertiesService.setUserProperty(TRIGGER_VERSION, pkg.version);
+  return pkg.version;
+}
+
 function safeSend(email, subject, body, attachments) {
   try {
     GmailApp.sendEmail(email, subject, body, { attachments });
+    return true;
   } catch (error) {
     Logger.log({
       message: `failed to send ${email}: ${subject}`,
@@ -199,20 +272,36 @@ function safeSend(email, subject, body, attachments) {
       attachments,
       error
     });
+    return false;
   }
 }
 
 global.hourly = () => {
   const currentCheck = new Date().toISOString();
   const lastCheck = new Date(g.PropertiesService.getUserProperty(LAST_CHECK));
+  const lastCheckSummary: CheckSummary = {
+    folders: [],
+    attempted: 0,
+    sent: 0
+  };
   for (const id of getFolderIds()) {
     const folder = DriveApp.getFolderById(id);
+    const folderSummary: FolderSummary = {
+      name: folder.getName(),
+      id: folder.getId(),
+      files: []
+    };
     const config = g.PropertiesService.getUserProperty(id) as FolderConfig;
     const files = folder.getFiles(); // DriveApp is Drive v2, not v3
     const summaries = [];
     let attachments = config.attach ? [] : undefined;
     while (files.hasNext()) {
       const file = files.next();
+      const fileSummary: FileSummary = {
+        name: file.getName(),
+        id: file.getId(),
+        sent: false
+      };
       if (file.getDateCreated() >= lastCheck) {
         let summary = `${file.getName()}: ${file.getUrl()}`;
         if (attachments !== undefined) {
@@ -223,7 +312,8 @@ global.hourly = () => {
           }
         }
         if (config.separate) {
-          safeSend(
+          lastCheckSummary.attempted++;
+          fileSummary.sent = safeSend(
             config.email,
             `${file.getName()} added to ${folder.getName()}`,
             summary,
@@ -235,16 +325,26 @@ global.hourly = () => {
         } else {
           summaries.push(summary);
         }
+        folderSummary.files.push(fileSummary);
       }
       if (summaries.length) {
-        safeSend(
-          config.email,
-          `Files added to ${folder.getName}`,
-          summaries.join('\n\n'),
-          attachments
-        );
+        lastCheckSummary.attempted++;
+        if (
+          safeSend(
+            config.email,
+            `Files added to ${folder.getName}`,
+            summaries.join('\n\n'),
+            attachments
+          )
+        ) {
+          folderSummary.files.forEach((f) => (f.sent = true));
+          lastCheckSummary.sent++;
+        }
       }
     }
+    lastCheckSummary.folders.push(folderSummary);
   }
   g.PropertiesService.setUserProperty(LAST_CHECK, currentCheck);
+  g.PropertiesService.setUserProperty(LAST_CHECK_SUMMARY, lastCheckSummary);
+  Logger.log(lastCheckSummary);
 };
